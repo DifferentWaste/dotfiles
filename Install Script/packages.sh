eae19b4f9c4003922e48b9f6edac84481c3312ff
DifferentWaste <114049657+DifferentWaste@users.noreply.github.com>
2023-10-19 02:43:20 +0000

    packages.sh

--- Install Script/packages.sh
+++ Install Script/packages.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-sudo pacman -Syu amd-ucode arc-gtk-theme bitwarden btop dunst feh firefox firejail git gnome-boxes grim gtk4 gvfs htop hyprland hyprpaper iwd kate kde-gtk-config kitty materia-gtk-theme nano neofetch network-manager-applet networkmanager otf-font-awesome pacman-contrib plasma-{meta,desktop,wayland-protocols,wayland-session} qbittorrent swayidle thunar ttf-liberation ufw vi vim waybar wget wireless_tools wofi
+sudo pacman -Syu amd-ucode arc-gtk-theme bitwarden btop dunst feh firefox firejail git gnome-boxes grim gtk4 gvfs htop hyprland hyprpaper iwd kate kde-gtk-config kitty materia-gtk-theme nano neofetch network-manager-applet networkmanager otf-font-awesome pacman-contrib plasma-{meta,desktop,wayland-protocols,wayland-session} qbittorrent sddm swayidle thunar ttf-liberation ufw vi vim waybar wget wireless_tools wofi
 
 cd ~/Downloads
 
-git clone https://github.com/DifferentWaste/dotfiles/tree/main
-
 cd dotfiles
 
-cp dunst ~/.config/
-cp hypr ~/.config/
-cp kitty ~/.config/
-cp wallpapers ~/Pictures/
-cp waybar /etc/xdg/
+cp -r dunst ~/.config/
+cp hypr -r ~/.config/
+cp kitty -r ~/.config/
+cp wallpapers -r ~/Pictures/
+cp waybar -r /etc/xdg/
 
 echo "GOOD TO GO"
  
