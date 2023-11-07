449359934514d92b9cd629043df046583c2b7ea5
DifferentWaste <114049657+DifferentWaste@users.noreply.github.com>
2023-11-07 23:47:12 +0000

    packages.sh

--- Install Script/packages.sh
+++ Install Script/packages.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-sudo pacman -Syu amd-ucode arc-gtk-theme bitwarden btop dunst feh firefox firejail git gnome-boxes grim gtk4 gvfs htop hyprland hyprpaper iwd kate kde-gtk-config kitty materia-gtk-theme nano neofetch network-manager-applet networkmanager otf-font-awesome pacman-contrib plasma-{meta,desktop,wayland-protocols,wayland-session} qbittorrent sddm swayidle thunar ttf-liberation ufw vi vim waybar wget wireless_tools wofi
-
+sudo pacman -Syu amd-ucode arc-gtk-theme ark base base-devel bitwarden brave-bin btop btrfs-progs dolphindunst efibootmgr feh firefox firejail git gnome-boxesgrim gtk4 gvfs gvfs-gphoto2 gvfs-mtp htop hyprland hyprpaper iwd kate kde-gtk-config kitty kvantum kwayland-integration libreoffice-fresh  man-db man-pages materia-gtk-theme nanoneofetch nerdfetch network-manager-applet networkmanager openssh otf-font-awesome pacman-contrib plasma-desktop plasma-meta plasma-wayland-protocols plasma-wayland-session qbittorrent qt5ct qt6-wayland reflector sddm starship sway swayidle thunar thunar-volman ttf-firacode-nerd ttf-liberation udisks2-qt5 ufw veracrypt vi vim waybar wget wireless_tools wofi zram-generator 
 cd ~/Downloads
-
+git clone https://github.com/Differentwaste/dotfiles
 cd dotfiles
 
-cp -r dunst ~/.config/
-cp hypr -r ~/.config/
-cp kitty -r ~/.config/
-cp wallpapers -r ~/Pictures/
-cp waybar -r /etc/xdg/
+mv -r dunst ~/.config/
+mv -r hypr ~/.config/
+mv -r kitty ~/.config/
+mv -r wallpapers ~/Pictures/
+mv -r waybar /etc/xdg/
 
+cd ~/Downloads
+rmdir dotfiles
 cd
 mkdir aur
 cd /aur
@@ -20,5 +21,11 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -s -i -c
 
-echo "GOOD TO GO"
+echo "
+###  ### ### ##    ###  ###   ###  ###
+#    # # # # #  #   #   # #   #    # #
+# #  # # # # #  #   #   # #   # #  # #
+###  ### ### ##     #   ###   ###  ###
+"
+echo "Don't forget aur packages need manual install - per your discretion"
  
