fd52b4be2267c5e1533f6cd0152eff9fea85a858
DifferentWaste <114049657+DifferentWaste@users.noreply.github.com>
2023-12-06 23:55:07 +0000

    packages.sh

--- Install Script/packages.sh
+++ Install Script/packages.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-
-sudo pacman -Syu amd-ucode arc-gtk-theme ark base base-devel bitwarden brave-bin btop btrfs-progs dolphin dunst efibootmgr feh firefox firejail git gnome-boxes grim gtk4 gvfs gvfs-gphoto2 gvfs-mtp htop hyprland hyprpaper iwd kate kde-gtk-config kitty kvantum kwayland-integration libreoffice-fresh  man-db man-pages materia-gtk-theme nanoneofetch nerdfetch network-manager-applet networkmanager openssh otf-font-awesome pacman-contrib plasma-desktop plasma-meta plasma-wayland-protocols plasma-wayland-session qbittorrent qt5ct qt6-wayland reflector sddm starship sway swayidle thunar thunar-volman ttf-firacode-nerd ttf-liberation udisks2-qt5 ufw veracrypt vi vim waybar wget wireless_tools wofi zram-generator 
-mkdir -p ~/Pictures
-cd ~/Downloads
+cd ~/
 cd dotfiles
-mkdir -p ~/.config
-
-mv -r dunst ~/.config/
-mv -r hypr ~/.config/
-mv -r kitty ~/.config/
-mv -r wallpapers ~/Pictures/
-mv -r waybar /etc/xdg/
+mv dunst ~/.config
+mv hypr ~/.config
+mv kitty ~/.config
 mv starship.toml ~/.config
-
-cd ~/Downloads
-rmdir dotfiles
-cd
+mv waybar /etc/xdg/
+mv wallpapers ~/Pictures/
+cd ~/
+rmdir -r dotfiles
+sudo pacman -Syu amd-ucode arc-gtk-theme bitwarden btop dolphin dunst efibootmgr feh firefox firejail git gnome-boxes grim gtk4 gvfs gvfs-gphoto2 gvfs-mtp htop hyprland hyprpaper iwd kate kde-gtk-config kitty kvantum kwayland-integration libreoffice-fresh man-db man-pages materia-gtk-theme nano networkmanager otf-font-awesome pacman-contrib pavucontrol plasma-desktop plasma-meta plasma-wayland-protocols plasma-wayland-session pulseaudio pulseaudio-alsa pulsemixer qbittorrent qt5-wayland qt5ct qt6-wayland reflector smartmontools sof-firmware starship sway swayidle thunar thunar-volman ttf-firacode-nerd ttf-liberation udisks2-qt5 ufw veracrypt vim waybar wget wireless_tools wofi xdg-desktop-portal-hyprland xdg-utils 
 mkdir aur
 cd /aur
-
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -s -i -c --noconfirm
@@ -29,5 +22,7 @@ echo "
 # #  # # # # #  #   #   # #   # #  # #
 ###  ### ### ##     #   ###   ###  ###
 "
-echo "Don't forget aur packages need manual install - per your discretion"
+echo 
+"Don't forget aur packages need manual install - per your discretion
+You still need to configure Starship"
  
