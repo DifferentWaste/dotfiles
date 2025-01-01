eed8395579a3b986be060cf2e60200e0ebcb4b54
None <114049657+DifferentWaste@users.noreply.github.com>
2025-01-01 20:48:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ mv -v kitty ~/.config
 mv -v starship.toml ~/.config
 mv -v waybar ~/.config
 mv -v wallpapers ~/Pictures/
+mv -v qBittorrent ~/.config
 cd ~/
 
 sudo pacman -Syu amd-ucode arc-gtk-theme bitwarden btop breeze breeze-gtk dolphin dunst efibootmgr feh ffmpegthumbnailer firefox firejail git gnome-boxes grim gtk4 gvfs gvfs-gphoto2 gvfs-mtp htop hypridle hyprland hyprlang hyprlock hyprpaper hyprutils iwd kate kde-gtk-config kitty kvantum kwayland-integration libreoffice-fresh lxappearance man-db man-pages materia-gtk-theme nano networkmanager nwg-look otf-font-awesome pacman-contrib pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse qbittorrent qt5-wayland qt5ct qt6-wayland reflector smartmontools sof-firmware starship sway swayidle thunar thunar-volman ttf-firacode-nerd ttf-liberation tumbler udisks2-qt5 ufw veracrypt vim waybar wget wireless_tools wofi xdg-desktop-portal-hyprland xdg-utils 
